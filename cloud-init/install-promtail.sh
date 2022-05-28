640892790baebb2c314b738594ffa07f20884498
ion <ion@hashicorp.com>
2022-05-28 11:07:54 +0200

    Update install-promtail.sh

--- cloud-init/install-promtail.sh
+++ cloud-init/install-promtail.sh
@@ -7,6 +7,7 @@ until ping4 -c1 github.com ; do sleep 1; done
 # https://grafana.com/docs/loki/latest/installation/
 
 # get loki URL from using github release API
+which jq || apt-get install -y jq
 URL_PROMTAIL_LATEST=$(curl  -sS -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/grafana/loki/releases/latest | jq '.["assets"][] | select( ."name" | startswith("promtail-linux-amd64.zip")) | .["browser_download_url"]' | tr -d '"')
 
 # download loki binary
